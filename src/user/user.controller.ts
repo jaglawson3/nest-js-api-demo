5552a4b6e6932f5f56843f356056522e1becf3e5
Justin Lawson <jaglawson3@gmail.com>
2024-11-16 02:36:40 -0700

    Separated out auth guard and decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, UseGuards } from "@nestjs/common";
+import { User } from "@prisma/client";
+import { JwtGuard } from "src/auth/guard";
+import { GetUser } from "src/auth/decorator";
 
-
-@Controller('users')
+@UseGuards(JwtGuard)
+@Controller("users")
 export class UserController {
-    @UseGuards(AuthGuard('jwt'))
-    @Get('me')
-    getMe() {
-        return 'user info';
-    }
+  @Get("me")
+  getMe(@GetUser() user: User ) {
+    return user;
+  }
 }
