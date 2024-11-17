cbb0b1a20bc36776baca0b69461bbce4d074c852
Justin Lawson <jaglawson3@gmail.com>
2024-11-17 00:05:06 -0700

    Added majority of todo test cases

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,13 @@
-import { INestApplication, ValidationPipe } from '@nestjs/common';
-import { Test } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
 import { AppModule } from "../src/app.module";
-import { PrismaService } from "../src/prisma/prisma.service"
+import { PrismaService } from "../src/prisma/prisma.service";
+import * as pactum from "pactum";
 
 describe("App e2e", () => {
   let app: INestApplication;
   let prisma: PrismaService;
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -22,13 +24,53 @@ describe("App e2e", () => {
 
     prisma = app.get(PrismaService);
     await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333');
+    app.close()
   });
 
-  
-  
+  describe("Auth", () => {
+    describe("Signup", () => {
+      it.todo("should signup",
+        // () => {
+        //   const dto = {
+        //     email: "vlad@gmail.com",
+        //     password: "123",
+        //   };
+        //   return pactum
+        //     .spec()
+        //     .post("/auth/signup")
+        //     .withBody(dto)
+        //     .expectStatus(201)
+        //     .inspect();
+        // }
+        );
+    });
 
-  // afterAll(() => {
-  //   app.close();
+    describe("Signin", () => {
+      it.todo("should signin");
+    });
+  });
+
+  describe("User", () => {
+    describe("Get current user", () => {});
+
+    describe("Edit user", () => {});
+  });
+
+  describe("Bookmarks", () => {
+    describe("Create bookmark", () => {});
+
+    describe("Get bookmarks", () => {});
+
+    describe("Get bookmark by id", () => {});
+
+    describe("Edit bookmark", () => {});
+
+    describe("Delete bookmark", () => {});
+  });
+
+  // afterAll(async () => {
+  //   app.close();         
   // });
 
   it.todo("should pass");
